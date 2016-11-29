c397a022482da2d04237e003ea5b70f1ac537f31
IgorCanicali <igorbarcellos@ucl.br>
2016-11-29 10:19:09 -0200

    Update sqlcode.sql
    correção de erros de sintaxe

--- esquematico/sqlcode.sql
+++ esquematico/sqlcode.sql
@@ -1,30 +1,28 @@
 -- Geração de Modelo físico
 -- Sql ANSI 2003 - brModelo.
 
-
-
 CREATE TABLE fazenda (
 nome VARCHAR(50),
 id_faz INTEGER PRIMARY KEY
-)
+);
 
 CREATE TABLE região (
 nome VARCHAR(50),
 id_reg INTEGER PRIMARY KEY,
 id_faz INTEGER,
 FOREIGN KEY(id_faz) REFERENCES fazenda (id_faz)
-)
+);
 
 CREATE TABLE setor (
 id_set INTEGER PRIMARY KEY,
 id_reg INTEGER,
 FOREIGN KEY(id_reg) REFERENCES região (id_reg)
-)
+);
 
 CREATE TABLE cultura (
 nome VARCHAR(50),
 id_cul NUMERIC(10) PRIMARY KEY
-)
+);
 
 CREATE TABLE leitura (
 momento DATETIME,
@@ -33,7 +31,7 @@ id_sen INTEGER,
 id_set INTEGER,
 id_tip DECIMAL(10),
 FOREIGN KEY(id_set) REFERENCES setor (id_set)
-)
+);
 
 CREATE TABLE sensor (
 modelo VARCHAR(50),
@@ -41,12 +39,12 @@ fabricante VARCHAR(50),
 id_sen INTEGER PRIMARY KEY,
 id_set INTEGER,
 FOREIGN KEY(id_set) REFERENCES setor (id_set)
-)
+);
 
 CREATE TABLE tipo (
 id_tip DECIMAL(10) PRIMARY KEY,
 nome VARCHAR(50)
-)
+);
 
 CREATE TABLE localizacao (
 localizacao_PK INTEGER PRIMARY KEY,
@@ -55,9 +53,9 @@ latitude NUMERIC(10),
 longetude NUMERIC(10),
 id_set_FK INTEGER,
 FOREIGN KEY(id_set_FK) REFERENCES setor (id_set)
-)
+);
 
-CREATE TABLE parametros ideais_ (
+CREATE TABLE parametros_ideais (
 id_cul NUMERIC(10),
 id_tip DECIMAL(10),
 maximo DECIMAL(10),
@@ -66,7 +64,7 @@ baixo DECIMAL(10),
 minimo DECIMAL(10),
 FOREIGN KEY(id_cul) REFERENCES cultura (id_cul),
 FOREIGN KEY(id_tip) REFERENCES tipo (id_tip)
-)
+);
 
 CREATE TABLE plantio_ (
 id_set INTEGER,
@@ -75,14 +73,14 @@ data_plantio date,
 id_plan INTEGER PRIMARY KEY,
 FOREIGN KEY(id_set) REFERENCES setor (id_set),
 FOREIGN KEY(id_cul) REFERENCES cultura (id_cul)
-)
+);
 
 CREATE TABLE possui (
 id_tip DECIMAL(10),
 id_sen INTEGER,
 FOREIGN KEY(id_tip) REFERENCES tipo (id_tip),
 FOREIGN KEY(id_sen) REFERENCES sensor (id_sen)
-)
+);
 
-ALTER TABLE leitura ADD FOREIGN KEY(id_sen) REFERENCES sensor (id_sen)
-ALTER TABLE leitura ADD FOREIGN KEY(id_tip) REFERENCES tipo (id_tip)
+ALTER TABLE leitura ADD FOREIGN KEY(id_sen) REFERENCES sensor (id_sen);
+ALTER TABLE leitura ADD FOREIGN KEY(id_tip) REFERENCES tipo (id_tip);
