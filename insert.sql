d4be2dcfbb4015cee25fa315b3ec6eec923890de
IgorCanicali <igorbarcellos@ucl.br>
2016-11-29 10:51:29 -0200

    correção de sintaxe do sql

--- insert.sql
+++ insert.sql
@@ -2,28 +2,28 @@
 #VALUES (value1,value2,value3,...);
 
 INSERT INTO fazenda (nome,id_faz)
-VALUES (corrego do gato, 1),
-       (fortaleza, 2),
-       (Grapoama, 3),
-       (Santa helena, 4),
-       (cuiramá, 5),
-       (humm, 6),
-       (poiacá, 7),
-       (vale florido, 8),
-       (porto alegre, 9),
-       (amamatiaca, 10);
+VALUES ('corrego do gato', 1),
+       ('fortaleza', 2),
+       ('Grapoama', 3),
+       ('Santa helena', 4),
+       ('cuiramá', 5),
+       ('humm', 6),
+       ('poiacá', 7),
+       ('vale florido', 8),
+       ('porto alegre', 9),
+       ('amamatiaca', 10);
 
 INSERT INTO região (nome, id_reg, id_faz)
-VALUES (eucalipto, 1, 1),
-       (lagoa de tras, 2, 1),
-       (canavial, 3, 3),
-       (videira da pedra, 4, 4),
-       (café novo, 5, 3),
-       (cacau ema, 6, 3),
-       (cafe mineira, 7, 2),
-       (cafe morro, 8, 2),
-       (tulipas, 9, 8),
-       (viveiro, 10, 3);
+VALUES ('eucalipto', 1, 1),
+       ('lagoa de tras', 2, 1),
+       ('canavial', 3, 3),
+       ('videira da pedra', 4, 4),
+       ('café novo', 5, 3),
+       ('cacau ema', 6, 3),
+       ('cafe mineira', 7, 2),
+       ('cafe morro', 8, 2),
+       ('tulipas', 9, 8),
+       ('viveiro', 10, 3);
 
 INSERT INTO setor (id_set, id_reg)
 VALUES (1, 1),
@@ -38,40 +38,40 @@ VALUES (1, 1),
        (10, 2);
 
 INSERT INTO cultura (nome, id_cul)
-VALUES (cafe, 1),
-       (cacau, 2),
-       (maconha, 3),
-       (eucalipto, 4),
-       (tulipa, 5),
-       (laranja, 6),
-       (manga, 7),
-       (jaca, 8),
-       (pitanga, 9),
-       (alface, 10);
+VALUES ('cafe', 1),
+       ('cacau', 2),
+       ('maconha', 3),
+       ('eucalipto', 4),
+       ('tulipa', 5),
+       ('laranja', 6),
+       ('manga', 7),
+       ('jaca', 8),
+       ('pitanga', 9),
+       ('alface', 10);
 
 INSERT INTO leitura (momento, valor, id_sen, id_set, id_tip)
-VALUES (27/05/2000 13:45:56, 17, 1, 1, 4),
-       (27/05/2000 13:45:56, 18, 2, 2, 9),
-       (27/05/2000 13:75:56, 23, 3, 5, 3),
-       (27/05/2000 13:45:66, 78, 4, 6, 9),
-       (27/05/2000 13:45:56, 42, 5, 9, 10),
-       (32/05/2000 13:45:56, 9, 6, 10, 10),
-       (27/05/2000 25:45:56, 67, 7, 2, 3),
-       (27/05/2000 13:45:56, 84, 8, 6, 9),
-       (27/13/2000 13:45:56, 5, 9, 10, 6),
-       (27/05/2100 13:45:56, 3, 10, 6, 4);
+VALUES ('27-05-2000 13:45:56', 17, 1, 1, 4),
+       ('27-05-2000 13:45:56', 18, 2, 2, 9),
+       ('27-05-2000 13:75:56', 23, 3, 5, 3),
+       ('27-05-2000 13:45:66', 78, 4, 6, 9),
+       ('27-05-2000 13:45:56', 42, 5, 9, 10),
+       ('32-05-2000 13:45:56', 9, 6, 10, 10),
+       ('27-05-2000 25:45:56', 67, 7, 2, 3),
+       ('27-05-2000 13:45:56', 84, 8, 6, 9),
+       ('27-13-2000 13:45:56', 5, 9, 10, 6),
+       ('27-05-2100 13:45:56', 3, 10, 6, 4);
 
 INSERT INTO tipo (id_tip, nome)
-VALUES (1, nivel),
-       (2, humidade),
-       (3, temperatura),
-       (4, preciptação),
-       (5, capacitancia),
-       (6, insolação),
-       (7, indutividade),
-       (8, oxigenio),
-       (9, nitrogenio),
-       (10, fosforo);
+VALUES (01, 'nivel'),
+       (02, 'humidade'),
+       (03, 'temperatura'),
+       (04, 'preciptação'),
+       (05, 'capacitancia'),
+       (06, 'insolação'),
+       (07, 'indutividade'),
+       (08, 'oxigenio'),
+       (09, 'nitrogenio'),
+       (10, 'fosforo');
 
 INSERT INTO parametros (id_cul, id_tip, maximo, alto, baixo, minimo)
 VALUES (1, 1, 60, 55, 50, 35),
@@ -86,16 +86,16 @@ VALUES (1, 1, 60, 55, 50, 35),
        (8, 1, 67, 52, 47, 32);
 
 INSERT INTO sensor (id_sen, modelo, fabricante, id_set)
-VALUES (01, a, c, 3),
-       (02, a, b, 8),
-       (03, c, d, 3),
-       (04, d, c, 5),
-       (05, b, a, 1),
-       (06, d, d, 6),
-       (07, b, b, 5),
-       (08, b, d, 6),
-       (09, c, d, 1),
-       (10, a, b, 1);
+VALUES (01, 'a', 'c', 3),
+       (02, 'a', 'b', 8),
+       (03, 'c', 'd', 3),
+       (04, 'd', 'c', 5),
+       (05, 'b', 'a', 1),
+       (06, 'd', 'd', 6),
+       (07, 'b', 'b', 5),
+       (08, 'b', 'd', 6),
+       (09, 'c', 'd', 1),
+       (10, 'a', 'b', 1);
 
 INSERT INTO localizacao (localizacao_PK, altitude, latitude, longetude, id_set_FK)
 VALUES (01, 34.53, -19.8),
@@ -110,16 +110,16 @@ VALUES (01, 34.53, -19.8),
        (10, 31.27, -19.8);
 
 INSERT INTO plantio_ (id_plan, id_set, id_cul, data_plantio)
-VALUES (01, 10, 01, 07/01/2016),
-       (02, 05, 09, 05/12/2012),
-       (03, 06, 07, 19/04/1918),
-       (04, 10, 06, 20/04/2004),
-       (05, 05, 04, 14/06/2002),
-       (06, 05, 10, 08/09/2004),
-       (07, 10, 02, 03/05/2012),
-       (08, 04, 07, 14/10/2016),
-       (09, 09, 70, 17/02/2009),
-       (10, 06, 04, 16/03/2016);
+VALUES (01, 10, 01, '07-01-2016'),
+       (02, 05, 09, '05-12-2012'),
+       (03, 06, 07, '19-04-1918'),
+       (04, 10, 06, '20-04-2004'),
+       (05, 05, 04, '14-06-2002'),
+       (06, 05, 10, '08-09-2004'),
+       (07, 10, 02, '03-05-2012'),
+       (08, 04, 07, '14-10-2016'),
+       (09, 09, 70, '17-02-2009'),
+       (10, 06, 04, '16-03-2016');
 
 INSERT INTO possui (id_sen, id_tip)
 VALUES (03, 08),
